f59824a216bdfbe6484e47156d3fd8e8dcda2a9b
Vivek Goswami <54740376+falconfused@users.noreply.github.com>
2023-02-20 17:56:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,57 @@
-
-// let x = localStorage.getItem("key");
-// if (!x) {
-//     document.body.innerHTML = x
-// }
-
-
-
-
-
-
 function isValidPassword(password) {
     const regexPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
     return regexPattern.test(password);
 }
 
-let userinput;
-let passworddiv;
-let login;
-let logout;
-let success;
-let failure;
+let isLoggedIn = false;
 
+if (window.localStorage.getItem("isLoggedIn") != null) {
+    isLoggedIn = window.localStorage.getItem("isLoggedIn")
+    console.log(isLoggedIn);
+    if (isLoggedIn === "true"){
+        console.log("asd")
+        document.getElementById('userinput').style.display = "none";
 
+        document.getElementById('passworddiv').style.display = "none";
 
+        document.getElementById('login').style.display = "none";
 
-if (window.localStorage.getItem("userinput")) {
-    document.getElementById('userinput').style.display = window.localStorage.getItem("userinput")
+        document.getElementById('logout').style.display = "flex";
+
+        document.getElementById('success').style.display = "flex"
+
+        document.getElementById('failure').style.display = "none"
+
+    }
+
+    else {
+        document.getElementById('userinput').style.display = "block";
+        document.getElementById('passworddiv').style.display = "block";
+        document.getElementById('login').style.display = "flex";
+        document.getElementById('logout').style.display = "none";
+        document.getElementById('success').style.display = "none"
+        document.getElementById('failure').style.display = "none"
+    }
 }
-if (window.localStorage.getItem("passworddiv")) {
-    document.getElementById('passworddiv').style.display = window.localStorage.getItem("passworddiv")
-}
-if (window.localStorage.getItem("login")) {
-    document.getElementById('login').style.display = window.localStorage.getItem("login")
-}
-if (window.localStorage.getItem("logout")) {
-    document.getElementById('logout').style.display = window.localStorage.getItem("logout")
-}
-if (window.localStorage.getItem("success")) {
-    document.getElementById('success').style.display = window.localStorage.getItem("success")
-}
-if (window.localStorage.getItem("failure")) {
-    document.getElementById('failure').style.display = window.localStorage.getItem("failure")
-}
-
-window.localStorage.setItem("passworddiv", document.getElementById('passworddiv').style.display);
-window.localStorage.setItem("login", document.getElementById('login').style.display);
-window.localStorage.setItem("success", document.getElementById('success').style.display);
-window.localStorage.setItem("logout", document.getElementById('logout').style.display);
-window.localStorage.setItem("failure", document.getElementById('failure').style.display);
-
-
-
 
 function check() {
     let password = document.getElementById('password').value;
     let username = document.getElementById('user').value
-    console.log(username)
+    // console.log(username)
     if (isValidPassword(password) == true && (username != "")) {
         document.getElementById('userinput').style.display = "none";
-        userinput = "none";
         document.getElementById('passworddiv').style.display = "none";
-        passworddiv = "none";
         document.getElementById('login').style.display = "none";
-        login = "none"
         document.getElementById('logout').style.display = "flex";
-        logout = "flex"
         document.getElementById('success').style.display = "flex"
-        success = "flex"
         document.getElementById('failure').style.display = "none"
-        failure = "none"
+        isLoggedIn = true
         setstorage()
     }
     else {
         document.getElementById('failure').style.display = "flex"
         failure = "flex"
+        isLoggedIn = false
         setstorage()
     }
 }
@@ -92,26 +70,16 @@ function redirect() {
     logout = "none"
     success = "none"
     failure = "none"
-
+    isLoggedIn = false
     setstorage()
-
-
-
-
 }
-
-console.log(toString(document.getElementById('userinput').style.display))
 
 
 
 
 function setstorage() {
-    window.localStorage.setItem("userinput", userinput);
-    window.localStorage.setItem("passworddiv", passworddiv);
-    window.localStorage.setItem("login", login);
-    window.localStorage.setItem("success", success);
-    window.localStorage.setItem("logout", logout);
-    window.localStorage.setItem("failure", failure);
+    window.localStorage.setItem("isLoggedIn", isLoggedIn);
+setItem("failure", failure);
 
 }
 
